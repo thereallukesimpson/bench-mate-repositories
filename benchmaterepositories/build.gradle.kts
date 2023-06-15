0ecd4f9133ba90d48aaecceb7697dbe130db39d6
Luke Simpson <thereallukesimpson@gmail.com>
2023-06-15 17:44:02 +1000

    Add jvm() gets everything working!

--- benchmaterepositories/build.gradle.kts
+++ benchmaterepositories/build.gradle.kts
@@ -5,13 +5,14 @@ plugins {
 }
 
 group = "app.benchmate"
-version = "0.0.3"
+version = "0.0.4"
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     targetHierarchy.default()
 
     android {
+        jvm()
         compilations.all {
             kotlinOptions {
                 kotlinOptions { jvmTarget = JavaVersion.VERSION_1_8.toString() }
@@ -19,15 +20,15 @@ kotlin {
         }
     }
     
-//    listOf(
-//        iosX64(),
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach {
-//        it.binaries.framework {
-//            baseName = "benchmaterepositories"
-//        }
-//    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "benchmaterepositories"
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
