c534429fadf0f950ee1da6155eed6926bca4c04f
Luke Simpson <thereallukesimpson@gmail.com>
2023-06-21 20:15:56 +1000

    First pass at domain models

---
+++ benchmaterepositories/src/commonMain/kotlin/app/benchmate/repositories/models/Models.kt
@@ -0,0 +1,35 @@
+package app.benchmate.repositories.models
+
+data class Team(
+    val teamId: String,
+    val teamName: String,
+    val players: List<Player>
+)
+
+data class Player(
+    val playerId: String,
+    val firstName: String,
+    val lastName: String,
+    val games: List<Game>
+)
+
+data class Game(
+    val gameId: String,
+    val season: String,
+    val round: Int,
+    val benchId: String,
+    val players: List<Player>
+)
+
+data class Bench(
+    val benchId: String,
+    val playerBenchTime: List<BenchItem>
+)
+
+data class BenchItem(
+    val playerId: String,
+    val gameId: String,
+    val startTime: Int = 0, // TODO use TimeMark or similar
+    val endTime: Int // TODO use TimeMark or similar
+)
+
