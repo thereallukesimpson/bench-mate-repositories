8f7bd386dcfc0e219a871ca995fd0516486f78e3
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-03-03 09:32:21 +1100

    Add player to DB and required enum adapter

--- benchmaterepositories/src/commonMain/kotlin/app/benchmate/repositories/db/Database.kt
+++ benchmaterepositories/src/commonMain/kotlin/app/benchmate/repositories/db/Database.kt
@@ -1,6 +1,31 @@
 package app.benchmate.repositories.db
 
+import app.benchmate.repositories.models.PlayerStatus
+import appbenchmaterepositories.db.Player
+import com.squareup.sqldelight.EnumColumnAdapter
+
 internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
-    private val database = AppDatabase(databaseDriverFactory.createDriver())
+    private val database = AppDatabase(
+        driver = databaseDriverFactory.createDriver(),
+        PlayerAdapter = Player.Adapter(
+            playerStatusAdapter = EnumColumnAdapter<PlayerStatus>()
+        )
+    )
     private val dbQuery = database.appDatabaseQueries
+
+    internal fun addPlayerToDatabase(
+        playerId: String,
+        firstName: String,
+        number: Int,
+        playerStatus: PlayerStatus,
+        onBenchCount: Int
+    ) {
+        dbQuery.insertPlayer(
+            playerId = playerId,
+            firstName = firstName,
+            number = number.toLong(),
+            playerStatus = playerStatus,
+            onBenchCount = onBenchCount.toLong()
+        )
+    }
 }
